7bfcbae8d0b6bd9cbeeb62c972c488744b527445
flamexia <flamexia@tencent.com>
2025-09-19 14:27:12 +0800

    chore: 移动 aidev_wxbot 插件文件到子目录

--- src/plugins/aidev_wxbot/aidev_wxbot/wxaibot/views.py
+++ src/plugins/aidev_wxbot/aidev_wxbot/wxaibot/views.py
@@ -10,8 +10,10 @@ import uuid
 from logging import getLogger
 
 import requests
+from blueapps.account.decorators import login_exempt
 from django.conf import settings
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.request import Request
@@ -23,6 +25,29 @@ from .decryption import WXBizJsonMsgCrypt
 from ..utils.rabbitmq import rabbitmq_client
 
 logger = getLogger(__name__)
+
+
+@login_exempt
+@csrf_exempt
+def to_wxbot_callback_path(request):
+    query_string = request.META.get("QUERY_STRING", "")
+    target_url = f"http://{settings.BK_APP_CODE}--wxbot.bkapp-{settings.BK_APP_CODE}-{settings.ENVIRONMENT}/callback/?{query_string}"
+    if request.method == "GET":
+        response = requests.get(
+            target_url,
+            headers=dict(request.headers),
+            data=request.body,
+        )
+    else:
+        response = requests.post(
+            target_url,
+            headers=dict(request.headers),
+            data=request.body,
+        )
+    return HttpResponse(
+        response.content,
+        status=response.status_code,
+    )
 
 
 class WxAiBotViewSet(ViewSet):
--- template/{{cookiecutter.project_name}}/bk_plugin/apis/urls.py
+++ template/{{cookiecutter.project_name}}/bk_plugin/apis/urls.py
@@ -1,39 +1,14 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse
 
 """
 此文件为蓝鲸插件默认加载的url文件，其它各定义模块都从此处导入
 1. 位置：bk_plugin_framework/services/bpf_service/urls.py
 2. 前辍：/plugin_api/
 """
-import requests
-from blueapps.account.decorators import login_exempt
-from django.conf import settings
+
 from django.urls import include, path, re_path
-from django.views.decorators.csrf import csrf_exempt
 
-
-@login_exempt
-@csrf_exempt
-def to_wxbot_callback_path(request):
-    query_string = request.META.get("QUERY_STRING", "")
-    target_url = f"http://{settings.BK_APP_CODE}--wxbot.bkapp-{settings.BK_APP_CODE}-{settings.ENVIRONMENT}/callback/?{query_string}"
-    if request.method == "GET":
-        response = requests.get(
-            target_url,
-            headers=dict(request.headers),
-            data=request.body,
-        )
-    else:
-        response = requests.post(
-            target_url,
-            headers=dict(request.headers),
-            data=request.body,
-        )
-    return HttpResponse(
-        response.content,
-        status=response.status_code,
-    )
-
-
-urlpatterns = (path("", include("agent.urls")), re_path(r"^wxbot_callback/?$", to_wxbot_callback_path))
+urlpatterns = (
+    path("", include("agent.urls")),
+    re_path(r"^wxbot_callback/?$", aidev_wxbot.wxaibot.views.to_wxbot_callback_path),
+)
